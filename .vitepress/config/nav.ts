411739e8deab731d65b9d661c40eeb2bc93c831e
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-01-18 19:11:35 +0700

    docs: update forum link to github discussions

--- .vitepress/config/nav.ts
+++ .vitepress/config/nav.ts
@@ -11,7 +11,7 @@ export const navEn: DefaultTheme.NavItem[] = [
     items: [
       { text: 'Changelog', link: 'https://github.com/mivodev/mivo/releases' },
       { text: 'Contributing', link: 'https://github.com/mivodev/mivo/blob/main/CONTRIBUTING.md' },
-      { text: 'Forum (Issues)', link: 'https://github.com/mivodev/mivo/issues' },
+      { text: 'Forum (Discussions)', link: 'https://github.com/mivodev/mivo/discussions' },
       { text: 'Star on GitHub', link: 'https://github.com/mivodev/mivo/stargazers' }
     ]
   }
@@ -28,7 +28,7 @@ export const navId: DefaultTheme.NavItem[] = [
     items: [
       { text: 'Catatan Rilis', link: 'https://github.com/mivodev/mivo/releases' },
       { text: 'Kontribusi', link: 'https://github.com/mivodev/mivo/blob/main/CONTRIBUTING.md' },
-      { text: 'Forum (Issues)', link: 'https://github.com/mivodev/mivo/issues' },
+      { text: 'Forum (Diskusi)', link: 'https://github.com/mivodev/mivo/discussions' },
       { text: 'Beri Bintang', link: 'https://github.com/mivodev/mivo/stargazers' }
     ]
   }
